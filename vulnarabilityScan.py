8286b50b8e1c8706a97cbac855c7cbbc473d14a2
Administrator <administrator@Weston-MBP.lan>
2024-04-13 22:40:00 -0500

    Try 4

--- vulnarabilityScan.py
+++ vulnarabilityScan.py
@@ -9,5 +9,6 @@ def vulnurabilityScanner():
     with Gmp(connection=connection, transform=transform) as gmp:
         gmp.authenticate('admin2', '31eba0a4-eb15-4118-8ca4-ed06e13a8329')
         #gmp.start_task(task_id='4f65b771-b598-4d72-8077-748040ccc08d')
-        pretty_print(gmp.get_task(task_id="1d0bb0f2-de24-4019-a425-babd0c7550ea"))
+        with open("Vulnaranility.txt",'a') as f:
+            print(pretty_print(gmp.get_results(task_id="1d0bb0f2-de24-4019-a425-babd0c7550ea")), file=f)
  
