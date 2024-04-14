57f7c50377a05883f434d91d5b77e537254decb4
Administrator <administrator@Weston-MBP.lan>
2024-04-13 22:33:14 -0500

    Try 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from portScan import portSanner
+#from portScan import portSanner
 from vulnarabilityScan import vulnurabilityScanner
-from intrusionDetection import start_sniffing
+#from intrusionDetection import start_sniffing
 host = "10.0.2.4"
 #portSanner(host=host)
 vulnurabilityScanner()
--- vulnarabilityScan.py
+++ vulnarabilityScan.py
@@ -9,5 +9,5 @@ def vulnurabilityScanner():
     with Gmp(connection=connection, transform=transform) as gmp:
         #gmp.authenticate('admin2', '31eba0a4-eb15-4118-8ca4-ed06e13a8329')
         #gmp.start_task(task_id='4f65b771-b598-4d72-8077-748040ccc08d')
-        print(gmp.get_task(task_id="1d0bb0f2-de24-4019-a425-babd0c7550ea"))
+        pretty_print(gmp.get_task(task_id="1d0bb0f2-de24-4019-a425-babd0c7550ea"))
  
