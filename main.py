fe937390daed1b4220c00ac02a506fa69b1f21df
Administrator <administrator@Weston-MBP.lan>
2024-04-13 22:26:41 -0500

    Try 1

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from portScan import portSanner
 from vulnarabilityScan import vulnurabilityScanner
 from intrusionDetection import start_sniffing
 host = "10.0.2.4"
-portSanner(host=host)
+#portSanner(host=host)
 vulnurabilityScanner()
