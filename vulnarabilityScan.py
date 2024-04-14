babc65190c385db3527ddef0c08caac9c09c5d38
Administrator <administrator@Weston-MBP.lan>
2024-04-13 22:47:03 -0500

    Try 7

--- vulnarabilityScan.py
+++ vulnarabilityScan.py
@@ -8,7 +8,6 @@ transform = EtreeTransform()
 def vulnurabilityScanner():
     with Gmp(connection=connection, transform=transform) as gmp:
         gmp.authenticate('admin2', '31eba0a4-eb15-4118-8ca4-ed06e13a8329')
-        #gmp.start_task(task_id='4f65b771-b598-4d72-8077-748040ccc08d')
-        with open("Vulnaranility.txt",'a') as f:
+        gmp.start_task(task_id='4f65b771-b598-4d72-8077-748040ccc08d')
+        with open("VulnerabilityScan.txt",'a') as f:
             pretty_print(gmp.get_results(task_id="1d0bb0f2-de24-4019-a425-babd0c7550ea"),file=f)
- 
